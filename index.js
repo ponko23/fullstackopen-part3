683057994bdf92766ddb516775a6a9cff2346f44
ponko23 <kohe.mizu@gmail.com>
2019-09-10 15:04:37 +0900

    3.3 finished

--- index.js
+++ index.js
@@ -5,41 +5,48 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 const persons = [
-    {
-        "persons": [
-          {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-          },
-          {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-          },
-          {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 3
-          },
-          {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423121",
-            "id": 4
-          }
-        ]
-      }
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423121",
+    id: 4
+  }
 ]
 
 app.get('/info', (req, res) => {
-    res.send(
-        `Phonebook has info for ${persons.length} people<br />
-        ${new Date().toString()}`
-        )
+  res.send(
+    `Phonebook has info for ${persons.length} people<br />
+    ${new Date().toString()}`
+  )
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 const PORT = 3001
