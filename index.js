56331cc820d3b1382b0d5d85a75017c1e516a326
ponko23 <kohe.mizu@gmail.com>
2019-09-10 21:21:59 +0900

    3.9 finished

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use(morgan((tokens, req, res) => {
@@ -112,7 +115,7 @@ app.delete('/api/persons/:id', (req, res) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
