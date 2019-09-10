90881bab4dfcaa25ad49a7dcb9528352698355a0
ponko23 <kohe.mizu@gmail.com>
2019-09-10 16:36:06 +0900

    3.8 finished

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 app.use(bodyParser.json())
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 let persons = [
   {
